801ef7527d54ec0048c9106bdff7ce30cbdf0eab
Stanley Kariuki <103861317+leestan306@users.noreply.github.com>
2024-06-02 07:02:56 +0300

    remove base route

--- vite.config.ts
+++ vite.config.ts
@@ -18,12 +18,12 @@ export default defineConfig({
         theme_color: "#4A00FF",
         icons: [
           {
-            src: "/vue-draggable-todos/android-chrome-192x192.png",
+            src: "/android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/vue-draggable-todos/android-chrome-512x512.png",
+            src: "/android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
